d115f469de526b86a0e15c0927dcddca41d0e0e1
Shivam Govind Rao <shivamgrao9161@gmail.com>
2023-06-10 07:52:00 +0530

    op

--- src/utils/serives.js
+++
@@ -1,3 +0,0 @@
-       // const API_KEY = `AIzaSyC3pY-L0ZqvvXR5MFg8BCCdnivkwagPYfo`;
-        // const END_POINT = `https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyC3pY-L0ZqvvXR5MFg8BCCdnivkwagPYfo`;
-            // const test = `https://api.openweathermap.org/data/2.5/weather?q=varanasi&appid=${API_key}`;
