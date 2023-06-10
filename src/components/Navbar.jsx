caf153b70207db9c9441d0503f0e6610d8014a83
Shivam Govind Rao <shivamgrao9161@gmail.com>
2023-06-10 07:51:04 +0530

    up

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,13 +37,12 @@ const Navbar = () => {
         // }
         // navigator.geolocation.getCurrentPosition(success, error);
 
-        // const API_KEY = `AIzaSyC3pY-L0ZqvvXR5MFg8BCCdnivkwagPYfo`;
-        // const END_POINT = `https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyC3pY-L0ZqvvXR5MFg8BCCdnivkwagPYfo`;
+
         const END_POINT = `https://ipapi.co/json`;
 
 
 
-        // const test = `https://api.openweathermap.org/data/2.5/weather?q=varanasi&appid=${API_key}`;
+
 
         try {
             let locationData = await fetch(END_POINT);
