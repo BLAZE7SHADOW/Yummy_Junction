588d89cd617d4f4f547ff0723d51547c9c684efd
Shivam Govind Rao <shivamgrao9161@gmail.com>
2023-06-21 22:42:11 +0530

    updated

--- src/components/RestaurantMenu.jsx
+++ src/components/RestaurantMenu.jsx
@@ -5,6 +5,7 @@ import Shimmer from "./Shimmer";
 import { AiFillStar } from "react-icons/ai";
 import { MdOutlineTimelapse } from "react-icons/md";
 import { HiOutlineCurrencyRupee } from "react-icons/hi";
+import { MENU_IMG_API } from "../utils/constant";
 // import { initialValue, reducerFunction } from "./reducer/reducer";
 
 const ResturantMenu = () => {
@@ -72,7 +73,7 @@ const ResturantMenu = () => {
     return (
         <>
             <div className="main w-full flex flex-col justify-center items-center">
-                <div className="mainBodyRestaurantMenu flex flex-col items-center bg-red-300 w-[50%]">
+                <div className="mainBodyRestaurantMenu flex flex-col items-center  w-[50%]">
                     <div className="firstresNameDetails flex justify-between w-full p-2">
                         <div className="left">
                             <h1 className="name text-lg font-bold font-open">{name}</h1>
@@ -89,7 +90,7 @@ const ResturantMenu = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="secondTimeAndOffers flex flex-col bg-yellow-300 w-full ">
+                    <div className="secondTimeAndOffers flex flex-col w-full ">
                         <div className="timeandprice w-full flex justify-start gap-5 p-3">
                             <div className="time flex items-center gap-2 font-open text-black/80 font-bold">
                                 <MdOutlineTimelapse className="timeicon text-2xl" />  <span className="dtime font-[750]">{deliveryTime} MINS</span>
@@ -116,53 +117,49 @@ const ResturantMenu = () => {
                         </div>
                     </div>
 
-                    <div className="items w-full flex flex-col gap-5 bg-orange-400">
-                        <div className="thirdRecommended bg-pink-500 w-full flex flex-col gap-5 cursor-pointer" >
+                    <div className="items w-full flex flex-col gap-5">
+                        <div className="thirdRecommended  w-full flex flex-col gap-4 cursor-pointer" >
 
-                            <div className="summaryData flex justify-between p-4 pt-6" onClick={() => setShowRecommended(!showRecommended)}>
-                                <span className="summary text-xl font-open font-bold"> Recommended ({(recommendedVeg ? (recommendedVeg?.length) : (recommendedNonVeg?.length))})</span>
+                            <div className="summaryData flex justify-between w-full bg-white p-4 pt-6" onClick={() => setShowRecommended(!showRecommended)}>
+                                <span className="summary text-xl font-open font-bold "> Recommended ({(recommendedVeg ? (recommendedVeg?.length) : (recommendedNonVeg?.length))})</span>
                                 <span>{showRecommended ? "-" : "+"}</span>
                             </div>
                             {
                                 showRecommended &&
                                 (recommendedVeg ? (recommendedVeg) : (recommendedNonVeg))?.map((res) => (
 
-                                    <div key={res.card.info.id} className=" w-full bg-green-400">
-                                        <div className="d">
-                                            <div className="name"> {(res?.card?.info?.itemAttribute?.vegClassifier) === "VEG" ? <img width="20" height="20" src="https://img.icons8.com/color/48/vegetarian-food-symbol.png" alt="vegetarian-food-symbol" /> : (res?.card?.info?.itemAttribute?.vegClassifier) === "NONVEG" ? <img width="20" height="20" src="https://img.icons8.com/color/48/non-vegetarian-food-symbol.png" alt="non-vegetarian-food-symbol" /> : ""}</div>
-                                            <div className="name">{res?.card?.info?.name}</div>
-                                            <div className="pricing flex w-2/3  items-center">
-                                                {
-                                                    (res?.card?.info?.variantsV2?.variantGroups)?.map((item) => ((item.variations)?.map((innerPrice) => innerPrice)))
-                                                        ?
-                                                        (res?.card?.info?.variantsV2?.variantGroups)?.map((item) => ((item.variations)?.map((innerPrice) => ((
-                                                            <div key={innerPrice.id} className="">
-                                                                {
-                                                                    innerPrice.name === 'Half' ?
-                                                                        <span>₹{innerPrice.price}</span> : ""
-                                                                }
-                                                            </div>
-                                                        ))))) :
-                                                        <div className="flex gap-16">
-                                                            <span>₹ {(res?.card?.info?.price / 100)}</span>
-                                                        </div>
-                                                }
-                                            </div>
-                                            {/* {(res?.card?.info?.variantsV2?.pricingModels)?.map((item) => ((
-
-
-                                                <div key={item.id} className="flex gap-16">
-                                                    <span>p{item.price}</span>
-
+                                    <div key={res.card.info.id} className=" w-full p-4 flex flex-col justify-center gap-4">
+                                        <div className="parent flex justify-between items-start ">
+                                            <div className="data w-[95%]">
+                                                <div className=" flex flex-col justify-between ">
+                                                    <div className="py-1"> {(res?.card?.info?.itemAttribute?.vegClassifier) === "VEG" ? <img width="20" height="20" src="https://img.icons8.com/color/48/vegetarian-food-symbol.png" alt="vegetarian-food-symbol" /> : (res?.card?.info?.itemAttribute?.vegClassifier) === "NONVEG" ? <img width="20" height="20" src="https://img.icons8.com/color/48/non-vegetarian-food-symbol.png" alt="non-vegetarian-food-symbol" /> : ""}</div>
+                                                    <div className="font-semibold">{res?.card?.info?.name}</div>
+                                                    <div className="pricing flex w-2/3  items-center">
+                                                        {
+                                                            (res?.card?.info?.variantsV2?.variantGroups)?.map((item) => ((item.variations)?.map((innerPrice) => innerPrice)))
+                                                                ?
+                                                                (res?.card?.info?.variantsV2?.variantGroups)?.map((item) => ((item.variations)?.map((innerPrice) => ((
+                                                                    <div key={innerPrice.id} className="">
+                                                                        {
+                                                                            innerPrice.name === 'Half' ?
+                                                                                <span>₹{innerPrice.price}</span> : ""
+                                                                        }
+                                                                    </div>
+                                                                ))))) :
+                                                                <div className="flex gap-16">
+                                                                    <span>₹ {(res?.card?.info?.price / 100)}</span>
+                                                                </div>
+                                                        }
+                                                    </div>
                                                 </div>
+                                                <div className="text-[13px] text-slate-500 ">{res?.card?.info?.description}</div>
+                                            </div>
+                                            <div className="  "><img className="rounded-2xl w-32 h-28 pb-2" src={MENU_IMG_API + res?.card?.info?.imageId} /></div>
 
-                                            )))} */}
 
-
-
-                                            <div className="name">{res?.card?.info?.description}</div>
                                         </div>
                                     </div>
+
                                 )
                                 )
                             }
