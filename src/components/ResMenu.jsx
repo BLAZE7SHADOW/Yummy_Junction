52e96dd91e54f36f917e2fd7e509e4dafa95699d
Shivam Govind Rao <shivamgrao9161@gmail.com>
2023-06-23 22:28:36 +0530

    menu

--- src/components/ResMenu.jsx
+++ src/components/ResMenu.jsx
@@ -42,17 +42,17 @@ const ResMenu = () => {
                         {
                             showAccordians?.map((it, index) => (
                                 (it?.card?.card?.title) &&
-                                <div key={index} className="div bg-green-400 m-5 p-6">
+                                <div key={index} className="div bg-green-400 m-4 ">
                                     {
                                         <>
                                             <div className=" flex flex-col gap-10 bg-red-500">
 
-                                                <span className="summary text-xl font-open font-bold " onClick={() => setShowRecommended(!showRecommended)}> {it?.card?.card?.title} </span>
-                                                {/* <span>{showRecommended ? "-" : "+"}</span> */}
+                                                <span className="summary text-xl font-open font-bold bg-blue-400" onClick={() => setShowRecommended(!showRecommended)}> {it?.card?.card?.title} {(it?.card?.card?.itemCards?.length) ? (it?.card?.card?.itemCards?.length) : ""} </span>
+
                                             </div>
                                             {
                                                 showRecommended &&
-                                                (it?.card?.card?.itemCards)?.map((res) => (
+                                                ((it?.card?.card?.itemCards) ? (it?.card?.card?.itemCards)?.map((res) => (
 
                                                     <div key={res.card.info.id} className=" w-full p-4 flex flex-col justify-center gap-4">
                                                         <div className="parent flex justify-between items-start ">
@@ -87,7 +87,51 @@ const ResMenu = () => {
                                                     </div>
 
                                                 )
+                                                ) : (it?.card?.card?.categories)?.map((ress) => (
+
+
+
+
+
+                                                    (ress?.itemCards)?.map((res) => (
+
+                                                        <div key={res.card.info.id} className=" w-full p-4 flex flex-col justify-center gap-4">
+                                                            <div className="parent flex justify-between items-start ">
+                                                                <div className="data w-[95%]">
+                                                                    <div className=" flex flex-col justify-between ">
+                                                                        <div className="py-1"> {(res?.card?.info?.itemAttribute?.vegClassifier) === "VEG" ? <img width="20" height="20" src="https://img.icons8.com/color/48/vegetarian-food-symbol.png" alt="vegetarian-food-symbol" /> : (res?.card?.info?.itemAttribute?.vegClassifier) === "NONVEG" ? <img width="20" height="20" src="https://img.icons8.com/color/48/non-vegetarian-food-symbol.png" alt="non-vegetarian-food-symbol" /> : ""}</div>
+                                                                        <div className="font-semibold">{res?.card?.info?.name}</div>
+                                                                        <div className="pricing flex w-2/3  items-center">
+                                                                            {
+                                                                                (res?.card?.info?.variantsV2?.variantGroups)?.map((item) => ((item.variations)?.map((innerPrice) => innerPrice)))
+                                                                                    ?
+                                                                                    (res?.card?.info?.variantsV2?.variantGroups)?.map((item) => ((item.variations)?.map((innerPrice) => ((
+                                                                                        <div key={innerPrice.id} className="">
+                                                                                            {
+                                                                                                innerPrice.name === 'Half' ?
+                                                                                                    <span>₹{innerPrice.price}</span> : ""
+                                                                                            }
+                                                                                        </div>
+                                                                                    ))))) :
+                                                                                    <div className="flex gap-16">
+                                                                                        <span>₹ {(res?.card?.info?.price / 100)}</span>
+                                                                                    </div>
+                                                                            }
+                                                                        </div>
+                                                                    </div>
+                                                                    <div className="text-[13px] text-slate-500 ">{res?.card?.info?.description}</div>
+                                                                </div>
+                                                                <div className="  "><img className="rounded-2xl w-32 h-28 pb-2" src={MENU_IMG_API + res?.card?.info?.imageId} /></div>
+
+
+                                                            </div>
+                                                        </div>
+
+                                                    ))
+
                                                 )
+
+                                                ))
 
                                             }
 
@@ -101,7 +145,7 @@ const ResMenu = () => {
 
                     </div>
                 </div>
-            </div>
+            </div >
         </>
     )
 }
