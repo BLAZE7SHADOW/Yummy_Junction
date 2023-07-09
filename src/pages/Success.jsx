a0f9aa6cc5d96527aea45a5d64767f7c3974796f
Shivam Govind Rao <shivamgrao9161@gmail.com>
2023-07-09 13:43:15 +0530

    img up

--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -1,7 +1,7 @@
 // import { useEffect } from "react"
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import success from "../IMAGES/success.jpg"
+
 
 const Success = () => {
     const navigate = useNavigate();
@@ -13,7 +13,7 @@ const Success = () => {
     }, [])
     return (
         <div className="inset-0 w-full flex justify-center items-center my-10 lg:mb-10">
-            <img className="lg:w-1/3" src={success} alt="" />
+            <img className="lg:w-1/3" src="https://i.postimg.cc/pdfdHx7V/success.jpg" alt="" />
         </div>
     )
 }
