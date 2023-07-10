fc2f47cf81cbad52b5fade9be3cf0ee12b5978d6
Shivam Govind Rao <shivamgrao9161@gmail.com>
2023-07-10 11:54:59 +0530

    up

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import logoo from "../IMAGES/LOGOO.png"
 import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
 import SearchIcon from '@mui/icons-material/Search';
 import PercentIcon from '@mui/icons-material/Percent';
@@ -55,20 +54,20 @@ const Navbar = () => {
     return (
         <div className="parent flex justify-center w-full ">
             <div className="navbar flex  justify-center items-center w-full  max-[800px]:justify-between" >
-                <div className="left  flex  justify-center items-center  gap-3 max-[1000px]:w-[50%] max-[660px]:w-full">
-                    <Link to="/" className="flex justify-center items-center pl-5"><div className="logo-container  p-1 max-[1000px]:w-full ">
-                        <img className="pt-2 logo w-44" src={logoo} alt="" />
+                <div className="left  flex  justify-center items-center  lg:gap-3 max-[1000px]:w-[60%] max-[660px]:w-full">
+                    <Link to="/" className="flex justify-center items-center pl-5"><div className="logo-container  lg:p-1 max-[1000px]:w-full ">
+                        <img className="pt-4 py-2 logo w-40 h-16 lg:w-52 lg:h-20" src="https://i.postimg.cc/qqtBMBMZ/YUMMY-JUCTION-LOGO.png" alt="" />
                     </div></Link>
-                    <div className="workplace flex  gap-2 items-center justify-start font-semibold p-2 m-2  max-[1000px]:w-[50%]">
+                    <div className="workplace flex  gap-2 items-center justify-start font-semibold p-2 m-2 max-[1000px]:w-[40%]">
                         <LocationOnOutlinedIcon className="location " />
-                        <p className="max-[710px]:text-sm "> {(location !== null ? (location?.city + ", " + location?.region + ", " + location?.country_name) : " ")}</p>
+                        <p className="text-xs lg:text-lg"> {(location !== null ? (location?.city + ", " + location?.region + ", " + location?.country_name) : " ")}</p>
                     </div>
                 </div>
 
 
 
 
-                <div className="nav-items flex  justify-center  items-center max-[1000px]:w-[50%]  pr-6 max-[660px]:hidden">
+                <div className="nav-items flex  justify-center items-center pr-6 max-[660px]:hidden">
                     <ul className="items flex  justify-center items-center gap-16 m-2 px-10">
                         <div className="search-btn flex  gap-[5px] justify-center items-center cursor-pointer p-2 max-[1000px]:hidden  "><SearchIcon /><div className="search">Search</div></div>
                         <div className="offer-btn flex  gap-[5px] justify-center items-center cursor-pointer p-2  max-[1000px]:hidden "><PercentIcon /><div className="offers">Offers</div></div>
